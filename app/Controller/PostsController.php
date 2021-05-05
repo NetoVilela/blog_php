ff7af80c2dfa93a461ce88adf41bab193c903ec4
NetoVilela <favneto.a.v.n@gmail.com>
2021-05-05 16:15:56 -0300

    Criando posts

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -4,8 +4,21 @@ class PostsController extends AppController{
     public $helpers = array('Html', 'Form');
     public $name = 'Posts';
 
-    function index(){
+    public function index(){
         $this->set('posts',$this->Post->find('all'));
+    }
+
+    public function view($id=null){
+        $this->set('post',$this->Post->findById($id));
+    }
+
+    public function add(){
+        if($this->request->is('post')){
+            if($this->Post->save($this->request->data)){
+                $this->Flash->success("Post criado com sucesso!");
+                $this->redirect(array('action'=>'index'));
+            }
+        }
     }
 
 }
